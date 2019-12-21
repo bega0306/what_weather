ef213e3bfd2b3e741d3f68679ca00562bb83c993
bega0306 <bega0306@gmail.com>
2019-12-21 22:42:06 +0900

    Revert "Create Weathe.js"
    This reverts commit 81b048670d348348887bc20834c75b424d34ba80.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, What Weather!</h1>
-      <Weather></Weather>
     </div>
   );
 }
