11a66ccdd0ea465ce7bdbbd7a9ba32051a8cc4ab
bega0306 <bega0306@gmail.com>
2019-12-21 22:47:08 +0900

    Create Weather.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, What Weather!</h1>
+      <Weather></Weather>
     </div>
   );
 }
